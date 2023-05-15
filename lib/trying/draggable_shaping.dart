4903c39c43437c61d15447d630613af753189224
SSH <12si47bun@gmail.com>
2023-05-16 00:01:01 +0900

    23.05.15 Block Multiple Color change.

--- lib/trying/draggable_shaping.dart
+++ lib/trying/draggable_shaping.dart
@@ -12,7 +12,13 @@ class DraggableShaping extends StatefulWidget {
 }
 
 class _DraggableShapingState extends State<DraggableShaping> {
-  Color originColor = Colors.red;
+  /// 변수 정의
+  Color originColor = Colors.red; // 드래그타켓 블록 시작 색상
+  Color firstColor = Colors.orangeAccent; // 드래그할 블록 색상 & 드래그되고 바뀔 색상
+  String moveBlockText = "Box";
+  
+  
+  
 
   @override
   Widget build(BuildContext context) {
@@ -29,39 +35,39 @@ class _DraggableShapingState extends State<DraggableShaping> {
             SizedBox(height: 10,),
             Row(
               children: [
-                _target(),
-                _target(),
-                _target(),
+                blockI(),
               ],
             ),
             Row(
               children: [
-                _target(),
+
               ],
             ),
             SizedBox(height: 10,),
             Draggable(
               /// 드롭되고 보여줄 색상
-                data: Colors.orangeAccent,
+                data: firstColor,
                 child: Container(
-                  width: 75,
-                  height: 75,
-                  color: Colors.orangeAccent,
+                  width: targetBlockWidth*0.75,
+                  height: targetBlockHeight*0.75,
+                  /// 드래그할 블록 색상
+                  color: firstColor,
+                  // color: Colors.orangeAccent,
 
-                  child: const Center(
+                  child: Center(
                     child: Text(
-                      "Box",
+                      moveBlockText,
                     ),
                   ),
                 ),
                 onDraggableCanceled: (velocity, offset) {},
                 feedback: Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.orangeAccent.withOpacity(0.5),
-                  child: const Center(
+                  width: targetBlockWidth,
+                  height: targetBlockHeight,
+                  color: firstColor.withOpacity(0.5),
+                  child: Center(
                     child: Text(
-                      "Box...",
+                      moveBlockText,
                       style: TextStyle(
                         color: Colors.white,
                         decoration: TextDecoration.none,
@@ -80,8 +86,16 @@ class _DraggableShapingState extends State<DraggableShaping> {
     );
   }
 
-  /// 드래그 될 부분 위젯으로 빼기
-  Widget _target(){
+  /// DragTarget 블록 변수 정의
+  double targetBlockWidth = 50;
+  double targetBlockHeight = 50;
+  double targetBlockPadding = 2;
+  String targetBlockText = "Drag Here";
+  Color targetBlockShade = Colors.grey.shade200;
+
+
+  /// 블록 종류 만들기
+  Widget blockI(){
     return DragTarget(
         /// onAccept: 드롭했을때 발생할 이벤트 표시.
         /// 여기선 originColor가 Draggable 위젯의 data 색상을 받아옴.
@@ -93,18 +107,66 @@ class _DraggableShapingState extends State<DraggableShaping> {
             List<dynamic> accepted,
             List<dynamic> rejected
             ) {
-          return Container(
-            width : 100,
-            height: 100,
-            /// DragTarget에 드롭되기 전에 비어있는(isEmpty) 상태에서 보여줄 색상 지정
-            color: accepted.isEmpty ? originColor : Colors.grey.shade200,
-            // decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-            child: const Center(
-              child: Text("Drag Here"),
-            ),
+          return Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(targetBlockPadding),
+                child: Container(
+                  width : targetBlockWidth,
+                  height: targetBlockHeight,
+                  /// DragTarget에 드롭되기 전에 비어있는(isEmpty) 상태에서 보여줄 색상 지정
+                  color: accepted.isEmpty ? originColor : targetBlockShade,
+                  // decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                  child: Center(
+                    child: Text(targetBlockText),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(targetBlockPadding),
+                child: Container(
+                  width : targetBlockWidth,
+                  height: targetBlockHeight,
+                  /// DragTarget에 드롭되기 전에 비어있는(isEmpty) 상태에서 보여줄 색상 지정
+                  color: accepted.isEmpty ? originColor : targetBlockShade,
+                  // decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                  child: Center(
+                    child: Text(targetBlockText),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(targetBlockPadding),
+                child: Container(
+                  width : targetBlockWidth,
+                  height: targetBlockHeight,
+                  /// DragTarget에 드롭되기 전에 비어있는(isEmpty) 상태에서 보여줄 색상 지정
+                  color: accepted.isEmpty ? originColor : targetBlockShade,
+                  // decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                  child: Center(
+                    child: Text(targetBlockText),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(targetBlockPadding),
+                child: Container(
+                  width : targetBlockWidth,
+                  height: targetBlockHeight,
+                  /// DragTarget에 드롭되기 전에 비어있는(isEmpty) 상태에서 보여줄 색상 지정
+                  color: accepted.isEmpty ? originColor : targetBlockShade,
+                  // decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                  child: Center(
+                    child: Text(targetBlockText),
+                  ),
+                ),
+              ),
+            ],
           );
         },
       );
-
   }
+
+
+
 }
